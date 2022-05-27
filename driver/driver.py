7ecf232e8a5b6d546827d7f65e0d94db648de54b
James Tan <jamestansx@gmail.com>
2022-05-27 19:30:40 +0800

    Implement youtube controller

---
+++ driver/driver.py
@@ -0,0 +1,37 @@
+from enum import Enum
+import serial
+import pyautogui
+import time
+
+PORT = "COM4"
+
+class Command(Enum):
+    PLAY = "space"
+    FASTF = "right"
+    REWIND = "left"
+    VOLUP = "volumeup"
+    VOLDOWN = "volumedown"
+
+if __name__ == "__main__":
+    try:
+        port = serial.Serial(
+                port=PORT,
+                baudrate=115200,
+                timeout=0.1,
+                write_timeout=0.1,
+        )
+    except serial.SerialException:
+        print("Cannot find port")
+
+    pyautogui.getWindowsWithTitle("Youtube")[0].maximize()
+    pyautogui.getWindowsWithTitle("Youtube")[0].activate()
+    while True:
+        try:
+            command = port.readline().decode().strip(" \r\n\b")
+            if command not in Command.__members__:
+                continue
+
+            pyautogui.press(Command[command].value)
+        except KeyboardInterrupt:
+            break
+
